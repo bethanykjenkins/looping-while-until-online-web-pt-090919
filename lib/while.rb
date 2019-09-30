9b3f32733ed883602e1d9ef536792b7e6d05a2a8
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-09-30 16:53:28 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,14 @@
 def using_while
   levitation_force = 6
-	
   #your code here
+while levitation_force < 10
+puts "Wingardium Leviosa"
+
+sleep (1) 
+end
+puts "HAPPY NEW YEAR"
+end 
+countdown_with_sleep
 end
 
 
